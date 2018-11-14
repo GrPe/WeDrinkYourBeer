180773c2e7b90dae01fb346b05a4555af22b844d
GrPe <grzesiek.p9804@gmail.com>
2018-11-14 15:42:13 +0100

    Enemy class - new fields

--- Game/core/src/GameObjects/Enemy.java
+++ Game/core/src/GameObjects/Enemy.java
@@ -1,18 +1,39 @@
 package GameObjects;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.ArrayList;
 
 public class Enemy extends GameObject implements Drawable
 {
 
     //Animation<TextureRegion> walkAnimation;
+    Sprite temp;
 
+    int hp;
+    float speed;
+    int damage;
+    ArrayList<Vector2> navLink;
+    int currectTarget;
+
+    public Enemy(Vector2 position, float rotation, Texture texture, ArrayList<Vector2> navLink, int hp, float speed, int damage) {
+        super(position, rotation);
+        temp = new Sprite(texture);
+        this.navLink = navLink;
+        this.damage = damage;
+        this.speed = speed;
+        this.hp = hp;
+        this.currectTarget = 0;
+    }
 
     @Override
-    public Sprite getDrawingSprite() {
-        return null;
+    public Sprite getDrawingSprite()
+    {
+        return temp;
     }
 
     @Override
@@ -24,4 +45,25 @@ public class Enemy extends GameObject implements Drawable
     public float getY() {
         return super.getPosition().y;
     }
+
+    public void dealDamage(int damage)
+    {
+        hp -= damage;
+    }
+
+    public void move()
+    {
+
+    }
+
+    public int getDamage()
+    {
+        return damage;
+    }
+
+    public boolean isDead()
+    {
+        return hp < 0;
+    }
+
 }
