a054de9754057c0d3790934d0d53581c41870cb1
GrPe <grzesiek.p9804@gmail.com>
2018-11-14 15:57:17 +0100

    Typos - Enemy

--- Game/core/src/GameObjects/Enemy.java
+++ Game/core/src/GameObjects/Enemy.java
@@ -2,9 +2,7 @@ package GameObjects;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 
@@ -18,7 +16,7 @@ public class Enemy extends GameObject implements Drawable
     private float speed;
     private int damage;
     private Vector2[] navLink;
-    private int currectTarget;
+    private int currentTarget;
 
     public Enemy(Vector2 position, float rotation, Texture texture, Vector2[] navLink, int hp, float speed, int damage) {
         super(position, rotation);
@@ -27,7 +25,7 @@ public class Enemy extends GameObject implements Drawable
         this.damage = damage;
         this.speed = speed;
         this.hp = hp;
-        this.currectTarget = 0;
+        this.currentTarget = 0;
     }
 
     @Override
@@ -53,8 +51,8 @@ public class Enemy extends GameObject implements Drawable
 
     public void move()
     {
-        float destX = navLink[currectTarget].x - super.getPosition().x;
-        float destY = navLink[currectTarget].y - super.getPosition().y;
+        float destX = navLink[currentTarget].x - super.getPosition().x;
+        float destY = navLink[currentTarget].y - super.getPosition().y;
 
         float dist = (float)Math.sqrt(destX * destX + destY * destY);
 
