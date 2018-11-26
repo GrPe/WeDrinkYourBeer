9c7ac9cb1a5c9d9ee5b685807582056f71b3906b
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2018-11-26 21:24:31 +0100

    Added Base class

---
+++ Game/core/src/GameObjects/Base.java
@@ -0,0 +1,55 @@
+package GameObjects;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+
+public class Base extends GameObject implements Drawable
+{
+    private Sprite sprite;
+    private int hp;
+
+    public Base(Vector2 position, Texture texture, int hp) {
+        super(position, 0);
+        this.sprite = new Sprite(texture);
+        this.hp = hp;
+    }
+
+    @Override
+    public Sprite getDrawingSprite() {
+        return sprite;
+    }
+
+    @Override
+    public float getX() {
+        return super.getPosition().x;
+    }
+
+    @Override
+    public float getY() {
+        return super.getPosition().y;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public void decreaseHp(int hp)
+    {
+        this.hp -= hp;
+    }
+
+    public boolean isDead()
+    {
+        return hp < 0;
+    }
+
+    public void increaseHp(int hp)
+    {
+        this.hp += hp;
+    }
+}
