ab4d8ec0d073a017df718567dcc0d8c2d80e4892
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2018-12-05 00:17:33 +0100

    Moved movement from class Enemy to State Machine

--- Game/core/src/FinityStateMachine/Enemy/WalkingState.java
+++ Game/core/src/FinityStateMachine/Enemy/WalkingState.java
@@ -1,5 +1,6 @@
 package FinityStateMachine.Enemy;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
 
 import FinityStateMachine.State;
@@ -9,11 +10,16 @@ import GameObjects.Enemy;
 public class WalkingState extends State {
 
     private Enemy enemy;
+    private Vector2 currentTarget;
+    private Vector2 finalTarget;
 
     public WalkingState(Enemy enemy)
     {
         this.enemy = enemy;
         stateID = StateID.EnemyWalking;
+
+        currentTarget = enemy.getNextTarget();
+        finalTarget = enemy.getFinalTarget();
     }
 
     @Override
@@ -31,7 +37,50 @@ public class WalkingState extends State {
     @Override
     public void Act()
     {
-        enemy.move();
+        if(!isInBase())
+        {
+            move();
+        }
+        else
+        {
+            enemy.SetInBase();
+        }
+    }
+
+
+    private void move()
+    {
+        float destX = currentTarget.x - enemy.getX();
+        float destY = currentTarget.y - enemy.getY();
+
+        float dist = (float)Math.sqrt(destX * destX + destY * destY);
+
+        destX = destX/dist;
+        destY = destY/dist;
+
+        enemy.setPosition(new Vector2(enemy.getX() + destX * enemy.getSpeed() * Gdx.graphics.getDeltaTime(),
+                enemy.getY() + destY * enemy.getSpeed() * Gdx.graphics.getDeltaTime()));
+
+        ChangePosition();
+    }
+
+
+    private void ChangePosition()
+    {
+        if(CheckPosition(1, currentTarget))
+        {
+            currentTarget = enemy.getNextTarget();
+        }
+    }
+
+    private boolean CheckPosition(int epsilon, Vector2 target)
+    {
+        return enemy.getPosition().epsilonEquals(target,epsilon);
+    }
+
+    private boolean isInBase()
+    {
+        return CheckPosition(2,finalTarget);
     }
 
 }
