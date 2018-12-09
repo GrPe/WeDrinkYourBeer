d18075c210911c3f0dad71b86e634e3e7c7e8f41
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2018-12-10 00:19:41 +0100

    Added Resource Manager

--- Game/core/src/Managers/ResourceManager.java
+++ Game/core/src/Managers/ResourceManager.java
@@ -1,6 +1,9 @@
 package Managers;
 
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreetypeFontLoader;
 
 public class ResourceManager
 {
@@ -9,24 +12,46 @@ public class ResourceManager
     public ResourceManager()
     {
         manager = new AssetManager();
-        LoadTextures();
         LoadFont();
         manager.finishLoading();
     }
 
-    private void LoadTextures()
+    public void LoadTextures(String path)
     {
-        //manager.load()
+        manager.load(path,Texture.class);
     }
 
     private void LoadFont()
     {
-
+        FreetypeFontLoader.FreeTypeFontLoaderParameter parameter = new FreetypeFontLoader.FreeTypeFontLoaderParameter();
+        parameter.fontFileName = "Fonts/Joystick.ttf";
+        parameter.fontParameters.size = 30;
+        parameter.fontParameters.borderStraight = true;
+        manager.load("Fonts/Joystick.ttf",BitmapFont.class,parameter);
     }
 
-
-    public AssetManager getManager()
+    public Texture GetTexture(String path)
     {
-        return manager;
+        if(manager.isLoaded(path))
+        {
+            return manager.get(path,Texture.class);
+        }
+        return null;
     }
+
+    public BitmapFont GetFont()
+    {
+        return manager.get("Fonts/Joystick.ttf",BitmapFont.class);
+    }
+
+    public void FinishLoading()
+    {
+        manager.finishLoading();
+    }
+
+    public void Clear()
+    {
+        manager.clear();
+    }
+
 }
