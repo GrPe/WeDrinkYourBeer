4e55f96228398aabfd6d68cdabb4b7622c9416e7
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2018-12-16 23:15:59 +0100

    Implemented resource manager in EnemyFactory -> not static now

--- Game/core/src/Factories/EnemyFactory.java
+++ Game/core/src/Factories/EnemyFactory.java
@@ -1,33 +1,41 @@
 package Factories;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 
+import GameObjects.Enemies.Student;
 import GameObjects.Enemy;
+import Managers.ResourceManager;
 
 public class EnemyFactory
 {
-    private static Vector2[] navLink = {
+    private Vector2[] navLink = {
             new Vector2(4*60,1*60),
             new Vector2(5*60,1*60),
             new Vector2(5*60,3*60),
             new Vector2(7*60,3*60),
             new Vector2(7*60,5*60),
             new Vector2(3*60,5*60)};
-    //temporary - Animation in future !!!
-    private static Texture studentTexture;
 
-    public static Enemy createStudentEnemy(Vector2 position)
+    private  ResourceManager resourceManager;
+
+    public EnemyFactory(ResourceManager resourceManager)
     {
-        if(studentTexture == null)
-        {
-            LoadStudentTexture();
-        }
-        return new Enemy(position,0,studentTexture,navLink,5,60,1);
+        this.resourceManager = resourceManager;
     }
-    private static void LoadStudentTexture()
+
+    public Enemy CreateEnemy(Vector2 position, EnemyType enemyType)
     {
-        studentTexture = new Texture(Gdx.files.internal("Enemies/enemy_test.png"));
+        Enemy enemy;
+        switch(enemyType)
+        {
+            case Student:
+                enemy = new Student(position, resourceManager.GetTexture("enemy_test.png"),navLink);
+                break;
+            default:
+                enemy = new Student(position, resourceManager.GetTexture("enemy_test.png"),navLink);
+                break;
+        }
+
+        return enemy;
     }
 }
