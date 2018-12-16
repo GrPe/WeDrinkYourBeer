721c84562f865b5e73376597472412015218c125
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2018-12-16 23:22:24 +0100

    Added Student class (like prefab in Unity)

---
+++ Game/core/src/GameObjects/Enemies/Student.java
@@ -0,0 +1,13 @@
+package GameObjects.Enemies;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+import GameObjects.Enemy;
+
+public class Student extends Enemy
+{
+    public Student(Vector2 position, Texture texture, Vector2[] navLink) {
+        super(position, 0, texture, navLink, 5, 60, 1);
+    }
+}
