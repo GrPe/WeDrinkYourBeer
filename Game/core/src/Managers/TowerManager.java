b899ab2930c9c585c484c23a6f7a75e1e1a8d601
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2019-01-02 18:37:09 +0100

    Added tower manager

---
+++ Game/core/src/Managers/TowerManager.java
@@ -0,0 +1,46 @@
+package Managers;
+
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.ArrayList;
+
+import Factories.TowerFactory;
+import Factories.TowerType;
+import GameObjects.Tower;
+
+public class TowerManager
+{
+    private ArrayList<Tower> towers;
+    private LevelManager levelManager;
+    private TowerFactory towerFactory;
+
+    public TowerManager(LevelManager levelManager, ResourceManager resourceManager)
+    {
+        towers = new ArrayList<Tower>();
+        this.levelManager = levelManager;
+        towerFactory = new TowerFactory(resourceManager);
+    }
+
+    public void SetTower(Vector2 position, TowerType towerType)
+    {
+        //if tower exists in this place
+        for(Tower t : towers)
+        {
+            if(t.getPosition().equals(position)) return;
+        }
+
+        if(levelManager.CheckIfTowerCanBePlaced(position))
+        {
+            towers.add(towerFactory.CreateTower(position,towerType));
+        }
+    }
+
+    public void Update()
+    {
+        for(Tower x : towers)
+        {
+            x.Update();
+        }
+    }
+
+}
