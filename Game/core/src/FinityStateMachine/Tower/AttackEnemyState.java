8ecbca4457227f0cdfcfba743d3738278f1c0a0c
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2019-01-03 17:53:15 +0100

    Added Attack state to Tower

--- Game/core/src/FinityStateMachine/Tower/AttackEnemyState.java
+++ Game/core/src/FinityStateMachine/Tower/AttackEnemyState.java
@@ -1,12 +1,16 @@
 package FinityStateMachine.Tower;
 
+import com.badlogic.gdx.Gdx;
+
 import FinityStateMachine.State;
 import FinityStateMachine.StateID;
+import FinityStateMachine.Transition;
 import GameObjects.Tower;
 
 public class AttackEnemyState extends State
 {
     private Tower tower;
+    float counter = 0;
 
     public AttackEnemyState(Tower tower)
     {
@@ -17,14 +21,24 @@ public class AttackEnemyState extends State
 
     @Override
     public void DoBeforeEntering() {
-
+        Gdx.app.log("test", "Enter Attack");
     }
 
     @Override
     public void DoBeforeLeaving() {
+        tower.setTarget(null);
     }
 
     @Override
-    public void Act() {
+    public void Act()
+    {
+        counter -= Gdx.graphics.getDeltaTime();
+        if(tower.getTarget().isDead()) tower.getStateMachine().PerformTransition(Transition.TowerWaitingFor);
+
+        if(counter <= 0)
+        {
+            tower.getTarget().dealDamage(tower.getDamage());
+            counter = tower.getFireSpeed();
+        }
     }
 }
