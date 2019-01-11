0709e992e04ae1e81f010b2f10d013cf5d728e80
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2019-01-11 15:30:46 +0100

    Added return to main menu button to ui manager

--- Game/core/src/Managers/UIManager.java
+++ Game/core/src/Managers/UIManager.java
@@ -12,6 +12,9 @@ public class UIManager
     private Label timerAndCounter;
     private Label coins;
 
+    //menu
+    private UIButton returnToMainMenuButton;
+
     //tower menu buttons
     private boolean towerMenuVisibility;
     private UIButton towerMenu;
@@ -24,7 +27,9 @@ public class UIManager
         towerMenuVisibility = false;
         baseHp = new Label(resourceManager.GetFont(), new Vector2(50,460),"12/12");
         timerAndCounter = new Label(resourceManager.GetFont(), new Vector2(350,460), "40");
-        coins = new Label(resourceManager.GetFont(), new Vector2(560,460),"120");
+        coins = new Label(resourceManager.GetFont(), new Vector2(560,440),"120");
+
+        returnToMainMenuButton = new UIButton(new Vector2(620,460),resourceManager.GetTexture("MainMenu.png"));
 
         towerMenu = new UIButton(new Vector2(0,0),resourceManager.GetTexture("towerMenu.png"));
         singleFireTowerButton = new UIButton(new Vector2(0,60),resourceManager.GetTexture("singleTowerMenuButton.png"));
@@ -52,6 +57,9 @@ public class UIManager
         baseHp.Render(batch);
         timerAndCounter.Render(batch);
         coins.Render(batch);
+
+        returnToMainMenuButton.Render(batch);
+
         towerMenu.Render(batch);
         if(towerMenuVisibility)
         {
@@ -82,6 +90,11 @@ public class UIManager
     {
         if(!towerMenuVisibility) return false;
         return harvesterTowerButton.IsClicked(position);
+    }
+
+    public boolean IsReturnToMainMenuButtonClicked(Vector2 position)
+    {
+        return returnToMainMenuButton.IsClicked(position);
     }
 
     public void SetVisibility(boolean state)
