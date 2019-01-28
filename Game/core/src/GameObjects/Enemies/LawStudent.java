361e3e07b65103d67186401b47ffbef8d291f677
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2019-01-28 20:56:53 +0100

    Added new enemy - Law Student

---
+++ Game/core/src/GameObjects/Enemies/LawStudent.java
@@ -0,0 +1,15 @@
+package GameObjects.Enemies;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.ArrayList;
+
+import GameObjects.Enemy;
+
+public class LawStudent extends Enemy
+{
+    public LawStudent(Vector2 position, Texture texture, ArrayList<Vector2> navLink) {
+        super(position, 0, texture, navLink, 200, 60, 1);
+    }
+}
