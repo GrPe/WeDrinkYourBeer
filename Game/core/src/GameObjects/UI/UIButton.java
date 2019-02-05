b99af7fd989ccae08c1ac6240e05c776a7ae0a9e
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2019-02-05 18:33:43 +0100

    Added click sound to button

--- Game/core/src/GameObjects/UI/UIButton.java
+++ Game/core/src/GameObjects/UI/UIButton.java
@@ -1,5 +1,6 @@
 package GameObjects.UI;
 
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 
@@ -7,15 +8,21 @@ import GameObjects.GameObject;
 
 public class UIButton extends GameObject
 {
-    public UIButton(Vector2 position, Texture texture) {
+    private Sound sound;
+
+    public UIButton(Vector2 position, Texture texture, Sound sound) {
         super(position, 0,texture);
+        this.sound = sound;
     }
 
     public boolean IsClicked(Vector2 position)
     {
-        return (position.x >= super.GetPosition().x &&
+        boolean results = (position.x >= super.GetPosition().x &&
                 position.x < super.GetPosition().x + sprite.getWidth() &&
                 position.y >= super.GetPosition().y &&
                 position.y < super.GetPosition().y + sprite.getHeight());
+        if(results)
+            sound.play(1.0f);
+        return results;
     }
 }
